394eba164a6bae615421f1fa3d2f2e06d08f1f38
planetabhijit <abhijitpatelraju@gmail.com>
2022-12-14 22:05:05 +0530

    adding records from db to UI

--- src/main/java/com/abhijit/myspringbatis/EmployeeController.java
+++ src/main/java/com/abhijit/myspringbatis/EmployeeController.java
@@ -2,16 +2,25 @@ package com.abhijit.myspringbatis;
 
 import javax.xml.ws.RequestWrapper;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.abhijit.myspringbatis.dao.EmployeeMapper;
 
 @Controller
 public class EmployeeController {
 
+	@Autowired
+	EmployeeMapper mapper;
+	
 	@RequestMapping("/")
 	public String index() {
 		
+		System.out.println(mapper.getAllEmployee());
 		return "list-employees";
 	}
+	
+	
 	
 }
--- src/main/java/com/abhijit/myspringbatis/dao/EmployeeMapper.java
+++ src/main/java/com/abhijit/myspringbatis/dao/EmployeeMapper.java
@@ -1,5 +1,24 @@
 package com.abhijit.myspringbatis.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import com.abhijit.myspringbatis.entity.Employee;
+import com.abhijit.myspringbatis.util.MyBatisUtil;
+
+@Repository
 public class EmployeeMapper {
 
+	public List<Employee> getAllEmployee(){
+		
+		SqlSession sqlSession = MyBatisUtil.getSqlSessionFactory().openSession();
+		List<Employee> employeeList = sqlSession.selectList("getAllEmployees");
+		sqlSession.commit();
+		sqlSession.close();
+		
+		return employeeList;
+	}
+	
 }
