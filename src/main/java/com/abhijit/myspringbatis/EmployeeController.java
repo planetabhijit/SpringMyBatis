9157abc0235ced6842922cbb1ed5e7db4fbb04c0
planetabhijit <abhijitpatelraju@gmail.com>
2022-12-16 22:30:24 +0530

    Handling the issue occured for showing the records from db

--- src/main/java/com/abhijit/myspringbatis/EmployeeController.java
+++ src/main/java/com/abhijit/myspringbatis/EmployeeController.java
@@ -5,6 +5,7 @@ import javax.xml.ws.RequestWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.abhijit.myspringbatis.dao.EmployeeMapper;
 
@@ -15,10 +16,12 @@ public class EmployeeController {
 	EmployeeMapper mapper;
 	
 	@RequestMapping("/")
-	public String index() {
+	public ModelAndView index() {
 		
-		System.out.println(mapper.getAllEmployee());
-		return "list-employees";
+	System.out.println(mapper.getAllEmployee());
+	ModelAndView mav = new ModelAndView("list-employees");
+	mav.addObject("list-employees", mapper.getAllEmployee());
+		return mav;
 	}
 	
 	
