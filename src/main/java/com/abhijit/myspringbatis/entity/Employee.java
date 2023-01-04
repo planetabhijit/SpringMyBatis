2958996c91431c7844c449e36cbe923810fa0df6
Abhijit Prasad <abhijit.prasad@dgsl.in>
2023-01-04 19:47:42 +0530

    Changes in xml file and dao class

--- src/main/java/com/abhijit/myspringbatis/entity/Employee.java
+++ src/main/java/com/abhijit/myspringbatis/entity/Employee.java
@@ -9,54 +9,67 @@ public class Employee {
 	private String hobbiees;
 	private String country;
 	private String address;
-	
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
+
 	public String getFullname() {
 		return fullname;
 	}
+
 	public void setFullname(String fullname) {
 		this.fullname = fullname;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getGender() {
 		return gender;
 	}
+
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
+
 	public String getHobbiees() {
 		return hobbiees;
 	}
+
 	public void setHobbiees(String hobbiees) {
 		this.hobbiees = hobbiees;
 	}
+
 	public String getCountry() {
 		return country;
 	}
+
 	public void setCountry(String country) {
 		this.country = country;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
-	
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
 	@Override
 	public String toString() {
 		return "Employee [id=" + id + ", fullname=" + fullname + ", email=" + email + ", gender=" + gender
 				+ ", hobbiees=" + hobbiees + ", country=" + country + ", address=" + address + "]";
 	}
-	
+
 }
